da7eaf6067f4715830945582c4eed594afca3ca3
smick00 <smick@emich.edu>
2022-11-11 20:40:48 -0500

    initializing screen3 for querying

--- screen3.php
+++ screen3.php
@@ -1,3 +1,10 @@
+<?php
+	require_once 'lib/common.php';
+	$conn = db_connect();
+	session_start();
+?>
+
+
 <!DOCTYPE HTML>
 <!-- Figure 3: Search Result Screen by Prithviraj Narahari, php coding: Alexander Martens -->
 <html>
@@ -35,7 +42,8 @@
 		<td style="width: 350px" colspan="3" align="center">
 			<div id="bookdetails" style="overflow:scroll;height:180px;width:400px;border:1px solid black;background-color:LightBlue">
 			<table>
-			<tr><td align='left'><button name='btnCart' id='btnCart' onClick='cart("123441", "", "Array", "all")'>Add to Cart</button></td><td rowspan='2' align='left'>iuhdf</br>By Avi Silberschatz</br><b>Publisher:</b> McGraw-Hill,</br><b>ISBN:</b> 123441</t> <b>Price:</b> 12.99</td></tr><tr><td align='left'><button name='review' id='review' onClick='review("123441", "iuhdf")'>Reviews</button></td></tr><tr><td colspan='2'><p>_______________________________________________</p></td></tr><tr><td align='left'><button name='btnCart' id='btnCart' onClick='cart("978-0316055437", "", "Array", "all")'>Add to Cart</button></td><td rowspan='2' align='left'>title</br>By fname lname</br><b>Publisher:</b> pub,</br><b>ISBN:</b> 978-0316055437</t> <b>Price:</b> 12.99</td></tr><tr><td align='left'><button name='review' id='review' onClick='review("978-0316055437", "title")'>Reviews</button></td></tr><tr><td colspan='2'><p>_______________________________________________</p></td></tr><tr><td align='left'><button name='btnCart' id='btnCart' onClick='cart("978-0345339706", "", "Array", "all")'>Add to Cart</button></td><td rowspan='2' align='left'>Lord of the Rings, The Fellowship of the</br>By J.R.R. Tolkien</br><b>Publisher:</b> Del Rey,</br><b>ISBN:</b> 978-0345339706</t> <b>Price:</b> 8.09</td></tr><tr><td align='left'><button name='review' id='review' onClick='review("978-0345339706", "Lord of the Rings, The Fellowship of the")'>Reviews</button></td></tr><tr><td colspan='2'><p>_______________________________________________</p></td></tr><tr><td align='left'><button name='btnCart' id='btnCart' onClick='cart("978-0590353427", "", "Array", "all")'>Add to Cart</button></td><td rowspan='2' align='left'>Harry Potter and the Sorcerer Stone</br>By J.K. Rowling</br><b>Publisher:</b> Scholastic,</br><b>ISBN:</b> 978-0590353427</t> <b>Price:</b> 8.47</td></tr><tr><td align='left'><button name='review' id='review' onClick='review("978-0590353427", "Harry Potter and the Sorcerer Stone")'>Reviews</button></td></tr><tr><td colspan='2'><p>_______________________________________________</p></td></tr>			</table>
+
+			</table>
 			</div>
 			
 			</td>

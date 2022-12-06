6ea2ab19ac78b71af67a5e6e2da32b8ae786a958
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-12-06 14:38:09 -0500

    Fix typo in user login
    Change query where clause to use `Username` instead of `username`

--- user_login.php
+++ user_login.php
@@ -5,17 +5,17 @@
 		$conn = db_connect();
 		session_start();
 
-		//grab and set variables
+		// grab and set variables
 		$username = $_POST["username"];
 		$pin = $_POST["pin"];
 		$DBusername = "";
 		$DBpin = "";
 
-		//query for any customer with the given username
-		$sql_query = "SELECT * FROM Customer WHERE username='$username';"; 
+		// query for any customer with the given username
+		$sql_query = "SELECT * FROM Customer WHERE Username='$username';"; 
 		$result = $conn->query($sql_query);
 
-		//grabbing the result from the DB
+		// grabbing the result from the DB
 		if(mysqli_num_rows($result) > 0 ){
 			while($row = mysqli_fetch_array($result)) {
 				$DBusername = $row['Username'];
@@ -23,7 +23,7 @@
 			}
 		}
 
-		//if all the inputs are valid, log the user in and send them to screen 2. otherwise stay here
+		// if all the inputs are valid, log the user in and send them to screen 2. otherwise stay here
 		if($DBusername == $username && $DBpin == $pin && $username != "" && $pin != "")
 		{
 			$_SESSION["user"] = $_POST["username"];

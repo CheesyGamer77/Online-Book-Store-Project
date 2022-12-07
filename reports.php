fc5f6dc0d1548eb0bc2a39989216763dd37bf9a1
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-12-06 20:07:04 -0500

    Add report for total books in each category

--- reports.php
+++ reports.php
@@ -10,11 +10,26 @@
         $conn = db_connect();
 		session_start();
 
+        // Get total registered customers
         $sql = "SELECT COUNT(*) AS customerCount FROM Customer;";
         $res = mysqli_query($conn, $sql);
         $customerCount = mysqli_fetch_assoc($res)['customerCount'];
-
-        // Get total registered customers
+        mysqli_free_result($res);
         echo "Total registered customers: $customerCount";
+
+        // Get total books in each category
+        $sql = "SELECT Genre, COUNT(*) AS total FROM Book;";
+        $res = mysqli_query($conn, $sql);
+        $totals = mysqli_fetch_assoc($res);
+        mysqli_free_result($res);
+
+        echo "<table><tr><th>Genre</th><th>Total</th></tr>";
+        foreach ($totals as $key => $value) {
+            echo "<tr><td>$key</td><td>$value</td></tr>";
+        }
+        echo "</table>";
+        
+
+		db_close($conn);
     ?></body>
 </html>

15f52a3e08569279862aff6ec3c55d5bf636dafe
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-12-07 12:30:22 -0500

    Start styling reports page a bit more

--- reports.php
+++ reports.php
@@ -1,79 +1,126 @@
+<?php
+    require_once 'lib/common.php';
+
+    $conn = db_connect();
+    session_start();
+
+    // Get total registered customers
+    $sql = "SELECT COUNT(*) AS customerCount FROM Customer;";
+    $res = mysqli_query($conn, $sql);
+    $customerCount = mysqli_fetch_assoc($res)['customerCount'];
+    mysqli_free_result($res);
+
+    // Get total books in each category
+    $sql = "SELECT Genre, COUNT(*) AS total
+        FROM Book
+        GROUP BY Genre
+        ORDER BY total DESC;";
+    $res = mysqli_query($conn, $sql);
+
+    $genreCounts = array();
+    while ($row = mysqli_fetch_array($res)) {
+        array_push($genreCounts, $row);
+    }
+    mysqli_free_result($res);
+    
+    // Get average monthly sales for the year, ordered by month
+    $sql = "SELECT Month, SUM(PurchaseTotal) AS MonthlySales
+        FROM (
+            SELECT MONTH(PurchasedAt) AS Month, Quantity * Price AS PurchaseTotal
+            FROM PurchaseOf, InPurchase
+            NATURAL JOIN Book
+            WHERE YEAR(PurchasedAt) = YEAR(NOW())
+        ) MonthlyTotal
+        GROUP BY Month
+        ORDER BY Month ASC";
+    $res = mysqli_query($conn, $sql);
+    
+    $total = 0;
+    $entries = 0;
+    $average = 0;
+    $yearlySales = array();
+    while ($row = mysqli_fetch_array($res)) {
+        array_push($yearlySales, $row);
+        $entries += 1;
+        $total += $row["MonthlySales"];
+    }
+    mysqli_free_result($res);
+
+    if ($entries > 0) {
+        $average = round($total / $entries, 2);
+    }
+
+    // Get all titles and number of reviews for each
+    $sql = "SELECT Title, COUNT(*) AS ReviewCount
+        FROM Book
+        NATURAL JOIN Review
+        GROUP BY ISBN;";
+    $res = mysqli_query($conn, $sql);
+
+    $reviewData = array();
+    while ($row = mysqli_fetch_array($res)) {
+        array_push($reviewData, $row);
+    }
+    mysqli_free_result($res);
+    db_close($conn);
+?>
+
 <!DOCTYPE HTML>
 <html>
     <head>
         <title>BBB Admin Reports</title>
     </head>
 
-    <body><?php
-        require_once 'lib/common.php';
+    <body>
+        <h1>Admin Reports</h1>
+        <h2>Total Registered Customers</h2>
+        <p><?php
+            echo $customerCount
+        ?></p>
 
-        $conn = db_connect();
-		session_start();
+        <h2>Books In Each Category</h2>
+        <table>
+            <tr>
+                <th>Genre</th>
+                <th>Total</th>
+            </tr>
+            <?php
+                foreach ($genreCounts as $row) {
+                    echo "<tr><td>" . $row["Genre"] . "</td><td>" . $row["total"] . "</td></tr>";
+                }
+            ?>
+        </table>
 
-        // Get total registered customers
-        $sql = "SELECT COUNT(*) AS customerCount FROM Customer;";
-        $res = mysqli_query($conn, $sql);
-        $customerCount = mysqli_fetch_assoc($res)['customerCount'];
-        mysqli_free_result($res);
-        echo "Total registered customers: $customerCount";
+        <h2>Yearly Sales</h2>
+        <h3>Totals By Month</h3>
+        <table>
+            <tr>
+                <th>Month</th>
+                <th>Total Sales</th>
+            </tr>
+            <?php
+                foreach ($yearlySales as $row) {
+                    echo "<tr><td>" . $row["Month"] . "</td><td>" . $row["MonthlySales"] . "</td></tr>";
+                }
+            ?>
+        </table>
 
-        // Get total books in each category
-        $sql = "SELECT Genre, COUNT(*) AS total
-            FROM Book
-            GROUP BY Genre
-            ORDER BY total DESC;";
-        $res = mysqli_query($conn, $sql);
+        <h3>Average Monthly Sales</h3>
+        <p><?php
+            echo "$" . $average;
+        ?></p>
 
-        echo "<table><tr><th>Genre</th><th>Total</th></tr>";
-        while ($row = mysqli_fetch_array($res)) {
-            echo "<tr><td>" . $row["Genre"] . "</td><td>" . $row["total"] . "</td></tr>";
-        }
-        echo "</table>";
-        mysqli_free_result($res);
-		
-        // Get average monthly sales for the year, ordered by month
-        $sql = "SELECT Month, SUM(PurchaseTotal) AS MonthlySales
-            FROM (
-                SELECT MONTH(PurchasedAt) AS Month, Quantity * Price AS PurchaseTotal
-                FROM PurchaseOf, InPurchase
-                NATURAL JOIN Book
-                WHERE YEAR(PurchasedAt) = YEAR(NOW())
-            ) MonthlyTotal
-            GROUP BY Month
-            ORDER BY Month ASC";
-        $res = mysqli_query($conn, $sql);
-        
-        echo "<table><tr><th>Month</th><th>Total Sales</th></tr>";
-        $total = 0;
-        $entries = 0;
-        $average = 0;
-        while ($row = mysqli_fetch_array($res)) {
-            $entries += 1;
-            $monthlySales = $row["MonthlySales"];
-            echo "<tr><td>" . $row["Month"] . "</td><td>" . $row["MonthlySales"] . "</td></tr>";
-            $total += $monthlySales;
-        }
-        echo "</table>";
-        mysqli_free_result($res);
-
-        if ($entries > 0) {
-            $average = round($total / $entries, 2);
-        }
-        echo "Average Sales Per Month: \$$average";
-
-        // Get all titles and number of reviews for each
-        $sql = "SELECT Title, COUNT(*) AS ReviewCount
-            FROM Book
-            NATURAL JOIN Review
-            GROUP BY ISBN;";
-        $res = mysqli_query($conn, $sql);
-
-        echo "<table><tr><th>Title</th><th>Total Reviews</th></tr>";
-        while ($row = mysqli_fetch_array($res)) {
-            echo "<tr><td>" . $row["Title"] . "</td><td>" . $row["ReviewCount"] . "</td></tr>";
-        }
-        echo "</table>";
-        mysqli_free_result($res);
-        db_close($conn);
-    ?></body>
+        <h2>Total Book Reviews By Title</h2>
+        <table>
+            <tr>
+                <th>Title</th>
+                <th>Total Reviews</th>
+            </tr>
+            <?php
+                foreach ($reviewData as $row) {
+                    echo "<tr><td>" . $row["Title"] . "</td><td>" . $row["ReviewCount"] . "</td></tr>";
+                }
+            ?>
+        </table>
+    </body>
 </html>

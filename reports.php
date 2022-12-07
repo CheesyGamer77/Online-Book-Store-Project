d20e9176cfaefb2f5845329561f3728d8d3fb276
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-12-06 19:11:34 -0500

    Add base reports page

---
+++ reports.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>BBB Admin Reports</title>
+    </head>
+
+    <body><?php
+        require_once 'lib/common.php';
+
+        $conn = db_connect();
+		session_start();        
+    ?></body>
+</html>

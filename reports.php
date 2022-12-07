b96d8a70d633849ba365069121e85142084320b3
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-12-06 19:41:33 -0500

    Add report for total customer count

--- reports.php
+++ reports.php
@@ -8,6 +8,13 @@
         require_once 'lib/common.php';
 
         $conn = db_connect();
-		session_start();        
+		session_start();
+
+        $sql = "SELECT COUNT(*) AS customerCount FROM Customer;";
+        $res = mysqli_query($conn, $sql);
+        $count = mysqli_fetch_assoc($res)['customerCount'];
+
+        // Get total registered customers
+        echo "Total registered customers: $customerCount";
     ?></body>
 </html>

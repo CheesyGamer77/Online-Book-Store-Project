bb08e5f15eb9e10df083e433bc98effae812ffaf
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-12-06 22:40:53 -0500

    Add report for average monthly sales

--- reports.php
+++ reports.php
@@ -19,9 +19,9 @@
 
         // Get total books in each category
         $sql = "SELECT Genre, COUNT(*) AS total
-        FROM Book
-        GROUP BY Genre
-        ORDER BY total DESC;";
+            FROM Book
+            GROUP BY Genre
+            ORDER BY total DESC;";
         $res = mysqli_query($conn, $sql);
 
         echo "<table><tr><th>Genre</th><th>Total</th></tr>";
@@ -29,8 +29,38 @@
             echo "<tr><td>" . $row["Genre"] . "</td><td>" . $row["total"] . "</td></tr>";
         }
         echo "</table>";
-        
+
         mysqli_free_result($res);
 		db_close($conn);
+
+        // Get average monthly sales for the year, ordered by month
+        $sql = "SELECT Month, SUM(PurchaseTotal) AS MonthlySales
+            FROM (
+                SELECT MONTH(PurchasedAt) AS Month, Quantity * Price AS PurchaseTotal
+                FROM PurchaseOf, InPurchase
+                NATURAL JOIN Book
+                WHERE YEAR(PurchasedAt) = YEAR(NOW())
+            ) MonthlyTotal
+            GROUP BY Month
+            ORDER BY Month ASC";
+        $res = mysqli_query($conn, $sql);
+        
+        echo "<table><tr><th>Month</th><th>Total Sales</th></tr>";
+        $total = 0;
+        $entries = 0;
+        $average = 0;
+        while ($row = mysqli_fetch_array($res)) {
+            $entries += 1;
+            $monthlySales = $row["MonthlySales"];
+            echo "<tr><td>" . $row["Month"] . "</td><td>" . $row["MonthlySales"] . "</td></tr>";
+            $total += $monthlySales;
+        }
+        echo "</table>";
+
+        if ($entries > 0) {
+            $average = round($total / $entries, 2);
+        }
+        echo "Average Sales Per Month: \$$average";
+
     ?></body>
 </html>

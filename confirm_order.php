4d89cb412fa13f1c2f8d122f34ca6ce0e7382571
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-12-08 14:29:58 -0500

    Use correct customer and shopping cart data

--- confirm_order.php
+++ confirm_order.php
@@ -1,3 +1,31 @@
+<?php
+	session_start();
+
+	if(!isset($_SESSION["username"]) || !isset($_SESSION["cart"])) {
+		header("Location: user_login.php");
+		exit;
+	}
+
+	$username = $_SESSION["username"];
+
+	require_once 'lib/common.php';
+	
+	$conn = db_connect();
+
+	$sql = "SELECT * FROM Customer
+		NATURAL JOIN (
+			SELECT CardNo, CardType, MONTH(ExpDate) AS ExpMonth, YEAR(ExpDate) as ExpYear)
+			FROM CreditCard
+		)CardData
+		WHERE Username='$username'";
+	$res = $conn->query($sql);
+
+	$customer = mysqli_fetch_assoc($res);
+	mysqli_free_result($res);
+
+	db_close($conn);
+?>
+
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -11,13 +39,13 @@
 		</tr>
 		
 		<!-- My guess is that this is for the customer name :shrug: -->
-		<td colspan="2">test test</td>
+		<td colspan="2"><?php echo $customer["FName"] . " " . $customer["LName"] ?></td>
 
 		<!-- Credit Card Deets -->
 		<!-- For some odd reason this is placed right after the start of the shipping address display :shrug: -->
 		<td rowspan="3" colspan="2">
 			<!-- You can choose to use the credit card already stored -->
-			<input type="radio" name="cardgroup" value="profile_card" checked>Use Credit card on file<br />MASTER - 1234567812345678 - 12/2015<br />
+			<input type="radio" name="cardgroup" value="profile_card" checked>Use Credit card on file<br /><?php echo $customer["CardType"] . " - " . $customer["CardNo"] . " - " . $customer["ExpMonth"] . "/" . $customer["ExpYear"] ?><br />
 			
 			<!-- Or use a new one instead -->
 			<input type="radio" name="cardgroup" value="new_card">New Credit Card<br />
@@ -34,9 +62,9 @@
 		</td>
 		
 		<!-- Here's the rest of the address stuff -->
-		<tr><td colspan="2">test</td></tr> <!-- Street -->
-		<tr><td colspan="2">test</td></tr> <!-- City -->
-		<tr><td colspan="2">Tennessee, 12345</td></tr> <!-- State, ZIP -->
+		<tr><td colspan="2"><?php echo $customer["Address"] ?></td></tr> <!-- Street -->
+		<tr><td colspan="2"><?php echo $customer["City"] ?></td></tr> <!-- City -->
+		<tr><td colspan="2"><?php echo $customer["State"] . ", " . $customer["Zip"] ?></td></tr> <!-- State, ZIP -->
 	
 		<tr>
 			<td colspan="3" align="center">
@@ -46,14 +74,13 @@
 						<th>Qty</th>
 						<th>Price</th>
 
-						<!-- Each book gets it's own row, similar to the below -->
-						<tr>
-							<td>
-								iuhdf</br><b>By</b> Avi Silberschatz</br><b>Publisher:</b> McGraw-Hill
-							</td>
-							<td>1</td>
-							<td>$12.99</td>
-						</tr>
+						<?php foreach ($_SESSION["cart"] as $book) {
+							echo "<tr>";
+							echo "<td>" . $book["title"] . "</br><b>By</b> " . $book["author"] . "</br><b>Publisher:</b> " . $book["publisher"] . "</td>";
+							echo "<td>" . $book["quantity"] . "</td>";
+							echo "<td>$" . $book["price"] . "</td>";
+							echo "</tr>";
+						}?>
 					</table>
 				</div>
 			</td>
@@ -67,9 +94,8 @@
 			</td>
 
 			<td align="right">
-				<!-- Totals! (Subtotal, Shipping Fee, Total) -->
 				<div id="bookdetails" style="overflow:scroll;height:180px;width:260px;border:1px solid black;">
-					SubTotal:$12.99</br>Shipping_Handling:$2</br>_______</br>Total:$14.99
+					SubTotal:$<?php echo $subtotal?></br>Shipping_Handling:$2</br>_______</br>Total:$<?php echo $subtotal + 2 ?>
 				</div>
 			</td>
 		</tr>

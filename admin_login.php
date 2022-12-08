4faf0456807678a9d545a668ad11e26199be4e25
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-12-08 13:05:52 -0500

    redo admin login

--- admin_login.php
+++ admin_login.php
@@ -6,28 +6,15 @@
 		session_start();
 
 		//grab and set variables
-		$username = $_POST["adminname"];
-		$pin = $_POST["pin"];
-		$DBusername = "";
-		$DBpin = "";
+		$username = mysqli_real_escape_string($conn, $_POST["adminname"]);
+		$pin = mysqli_real_escape_string($conn, $_POST["pin"]);
 
 		// query for any customer with the given username
-		$sql_query = "SELECT * FROM Admin WHERE Username='$username';"; 
-		$result = $conn->query($sql_query);
+		$sql = "SELECT * FROM Admin WHERE Username='$username' AND PIN='$pin;"; 
+		$result = $conn->query($sql);
 
 		// grabbing the result from the DB
 		if(mysqli_num_rows($result) > 0 ){
-			while($row = mysqli_fetch_array($result)) {
-				$DBusername = $row['Username'];
-				$DBpin = $row['PIN'];
-			}
-		}
-
-		alert($username);
-
-		// if all the inputs are valid, log the user in and send them to admin tasks. otherwise stay here
-		if($DBusername == $username && $DBpin == $pin && $username != "" && $pin != "")
-		{
 			$_SESSION["admin"] = $_POST["username"];
 			header("Location: admin_tasks.php");
 			exit;

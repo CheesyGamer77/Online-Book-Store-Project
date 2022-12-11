af139000541b4917378e5d80e90d1bc67273146f
smick00 <smick@emich.edu>
2022-12-10 19:41:46 -0500

    update_customerprofile now automatically loads existing customer data into fields

--- update_customerprofile.php
+++ update_customerprofile.php
@@ -1,18 +1,29 @@
 <?php
-	if (isset($_POST['register_submit'])) {
-		require_once 'lib/common.php';
-		session_start();
+	require_once 'lib/common.php';
+	$conn = db_connect();
+	//if a user that isn't logged in has somehow gotten here, send them to the login page
+	session_start();
+	if(!isset($_SESSION['username'])) {
+		header("Location: user_login.php");
+		exit;			
+	}
+	else
+		$username = $_SESSION["username"];
 
-		//if an user that isn't logged in has somehow gotten here, send them away
-		if(!isset($_SESSION['username'])) {
-			header("Location: user_login.php");
-			exit;
-		}
 
-		$conn = db_connect();
+	//grab all customer data, throw it into an assoc array
+	$sql = "
+	SELECT * 
+	FROM Customer 
+	NATURAL JOIN CreditCard 
+	WHERE Username='$username'";
+	$res = $conn->query($sql);
+	$customer = mysqli_fetch_assoc($res);
+	mysqli_free_result($res);
+
+	if (isset($_POST['update_submit'])) {
 
 		// use escape strings for inserting data
-		$username = mysqli_real_escape_string($conn, $_POST['username']);
 		$pin = mysqli_real_escape_string($conn, $_POST['pin']);
 		$retypePin = mysqli_real_escape_string($conn, $_POST['retype_pin']);
 		$firstName = mysqli_real_escape_string($conn, $_POST['firstname']);
@@ -24,11 +35,12 @@
 		$ccType = mysqli_real_escape_string($conn, $_POST['credit_card']);
 		$ccNumber = mysqli_real_escape_string($conn, $_POST['card_number']);
 		$ccExpiration = mysqli_real_escape_string($conn, $_POST['expiration']);
+
 	}
 
+	db_close($conn);
+
 ?>
-
-
 
 <!DOCTYPE HTML>
 <html>
@@ -41,20 +53,20 @@
 	<table align="center" style="border:2px solid blue;">
 		<tr>
 			<td align="right">
-				Username: 
+				Username: <?php echo($username) ?>
 			</td>
 			<td colspan="3" align="center">
 							</td>
 		</tr>
 		<tr>
 			<td align="right">
-				New PIN<span style="color:red">*</span>:
+				PIN<span style="color:red"></span>:
 			</td>
 			<td>
-				<input type="text" id="new_pin" name="new_pin">
+				<input type="text" id="new_pin" name="new_pin" value = <?php echo $customer["PIN"]?>>
 			</td>
 			<td align="right">
-				Re-type New PIN<span style="color:red">*</span>:
+				Re-type PIN<span style="color:red"></span>:
 			</td>
 			<td>
 				<input type="text" id="retypenew_pin" name="retypenew_pin">
@@ -62,77 +74,126 @@
 		</tr>
 		<tr>
 			<td align="right">
-				First Name<span style="color:red">*</span>:
+				First Name<span style="color:red"></span>:
 			</td>
 			<td colspan="3">
-				<input type="text" id="firstname" name="firstname">
+				<input type="text" id="firstname" name="firstname" value = <?php echo $customer["FName"]?>>
 			</td>
 		</tr>
 		<tr>
 			<td align="right"> 
-				Last Name<span style="color:red">*</span>:
+				Last Name<span style="color:red"></span>:
 			</td>
 			<td colspan="3">
-				<input type="text" id="lastname" name="lastname">
+				<input type="text" id="lastname" name="lastname" value = <?php echo $customer["LName"]?>>
 			</td>
 		</tr>
 		<tr>
 			<td align="right">
-				Address<span style="color:red">*</span>:
+				Address<span style="color:red"></span>:
 			</td>
 			<td colspan="3">
-				<input type="text" id="address" name="address">
+				<input type="text" id="address" name="address" value = <?php echo $customer["Address"]?>>
 			</td>
 		</tr>
 		<tr>
 			<td align="right">
-				City<span style="color:red">*</span>:
+				City<span style="color:red"></span>:
 			</td>
 			<td colspan="3">
-				<input type="text" id="city" name="city">
+				<input type="text" id="city" name="city" value = <?php echo $customer["City"]?>>
 			</td>
 		</tr>
 		<tr>
 			<td align="right">
-				State<span style="color:red">*</span>:
+				State<span style="color:red"></span>:
 			</td>
 			<td>
 				<select id="state" name="state">
-				<option selected disabled>select a state</option>
-				<option>Michigan</option>
-				<option>California</option>
-				<option>Tennessee</option>
+				<option selected><?php echo $customer["State"]?></option>
+					<option value="AL">Alabama</option>
+					<option value="AK">Alaska</option>
+					<option value="AZ">Arizona</option>
+					<option value="AR">Arkansas</option>
+					<option value="CA">California</option>
+					<option value="CO">Colorado</option>
+					<option value="CT">Connecticut</option>
+					<option value="DE">Delaware</option>
+					<option value="DC">District Of Columbia</option>
+					<option value="FL">Florida</option>
+					<option value="GA">Georgia</option>
+					<option value="HI">Hawaii</option>
+					<option value="ID">Idaho</option>
+					<option value="IL">Illinois</option>
+					<option value="IN">Indiana</option>
+					<option value="IA">Iowa</option>
+					<option value="KS">Kansas</option>
+					<option value="KY">Kentucky</option>
+					<option value="LA">Louisiana</option>
+					<option value="ME">Maine</option>
+					<option value="MD">Maryland</option>
+					<option value="MA">Massachusetts</option>
+					<option value="MI">Michigan</option>
+					<option value="MN">Minnesota</option>
+					<option value="MS">Mississippi</option>
+					<option value="MO">Missouri</option>
+					<option value="MT">Montana</option>
+					<option value="NE">Nebraska</option>
+					<option value="NV">Nevada</option>
+					<option value="NH">New Hampshire</option>
+					<option value="NJ">New Jersey</option>
+					<option value="NM">New Mexico</option>
+					<option value="NY">New York</option>
+					<option value="NC">North Carolina</option>
+					<option value="ND">North Dakota</option>
+					<option value="OH">Ohio</option>
+					<option value="OK">Oklahoma</option>
+					<option value="OR">Oregon</option>
+					<option value="PA">Pennsylvania</option>
+					<option value="RI">Rhode Island</option>
+					<option value="SC">South Carolina</option>
+					<option value="SD">South Dakota</option>
+					<option value="TN">Tennessee</option>
+					<option value="TX">Texas</option>
+					<option value="UT">Utah</option>
+					<option value="VT">Vermont</option>
+					<option value="VA">Virginia</option>
+					<option value="WA">Washington</option>
+					<option value="WV">West Virginia</option>
+					<option value="WI">Wisconsin</option>
+					<option value="WY">Wyoming</option>
+				</select>
 				</select>
 			</td>
 			<td align="right">
-				Zip<span style="color:red">*</span>:
+				Zip<span style="color:red"></span>:
 			</td>
 			<td>
-				<input type="text" id="zip" name="zip">
+				<input type="text" id="zip" name="zip" value = <?php echo $customer["Zip"]?>>
 			</td>
 		</tr>
 		<tr>
 			<td align="right">
-				Credit Card<span style="color:red">*</span>:
+				Credit Card<span style="color:red"></span>:
 			</td>
 			<td>
 				<select id="credit_card" name="credit_card">
-				<option selected disabled>select a card type</option>
+				<option selected><?php echo $customer["CardType"]?></option>
 				<option>VISA</option>
 				<option>MASTER</option>
 				<option>DISCOVER</option>
 				</select>
 			</td>
 			<td align="left" colspan="2">
-				<input type="text" id="card_number" name="card_number" placeholder="Credit card number">
+				<input type="text" id="card_number" name="card_number" value = <?php echo $customer["CardNo"]?>>
 			</td>
 		</tr>
 		<tr>
 			<td align="right" colspan="2">
-				Expiration Date<span style="color:red">*</span>:
+				Expiration Date<span style="color:red"></span>:
 			</td>
 			<td colspan="2" align="left">
-				<input type="text" id="expiration_date" name="expiration_date" placeholder="MM/YY">
+				<input type="text" id="expiration_date" name="expiration_date" value = <?php echo $customer["ExpDate"]?>>
 			</td>
 		</tr>
 		<tr>
@@ -140,7 +201,7 @@
 				<input type="submit" id="update_submit" name="update_submit" value="Update">
 			</td>
 			</form>
-		<form id="cancel" action="index.php" method="post">	
+		<form id="cancel" action="confirm_order.php" method="post">	
 			<td align="left" colspan="2">
 				<input type="submit" id="cancel_submit" name="cancel_submit" value="Cancel">
 			</td>
